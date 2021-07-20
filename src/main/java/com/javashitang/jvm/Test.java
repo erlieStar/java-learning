3c6efbb16a19d2e77b83ca4db8e9fbff39e00bd4
lilimin <290059742@qq.com>
2021-07-20 19:46:45 +0800

    增加字节码类

---
+++ src/main/java/com/javashitang/jvm/Test.java
@@ -0,0 +1,15 @@
+package com.javashitang.jvm;
+
+/**
+ * @author lilimin
+ * @since 2021-07-20
+ */
+public class Test {
+
+    public int calc() {
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return (a + b) * c;
+    }
+}
