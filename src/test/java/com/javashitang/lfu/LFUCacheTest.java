62ea4bf0cc10c6691f7a9224bb9bc2aed2400c7b
lilimin <290059742@qq.com>
2022-03-13 16:34:31 +0800

    完善lfu算法

--- src/test/java/com/javashitang/lfu/LFUCacheTest.java
+++ src/test/java/com/javashitang/lfu/LFUCacheTest.java
@@ -10,14 +10,14 @@ public class LFUCacheTest {
 
     @Test
     public void get() {
-        LFUCache<String, String> lfuCache = new LFUCache(2);
-        lfuCache.put("1", "1");
-        lfuCache.put("2", "2");
+        LFUCache lfuCache = new LFUCache(2);
+        lfuCache.put(1, 1);
+        lfuCache.put(2, 2);
         // 1
-        System.out.println(lfuCache.get("1"));
-        lfuCache.put("3", "3");
+        System.out.println(lfuCache.get(1));
+        lfuCache.put(3, 3);
         // 1的频率为2，2和3的频率为1，但2更早之前被访问，所以被清除
         // 结果为null
-        System.out.println(lfuCache.get("2"));
+        System.out.println(lfuCache.get(2));
     }
 }
